b3ca8c93b16436df73d46e63986e69e7c7168269
Andrey Morozov <andrey@morozoff.net>
2026-08-27 14:19:03 +0200

    Connect contact form to Formspree

--- main.js
+++ main.js
@@ -363,40 +363,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   );
 
-
-  /* =======================================================
-     LEAD FORM
-  ======================================================= */
-
-  const leadForm =
-    document.querySelector(
-      "#lead-form"
-    );
-
-  const formSuccess =
-    document.querySelector(
-      "#form-success"
-    );
-
-
-  if (
-    leadForm &&
-    formSuccess
-  ) {
-
-    leadForm.addEventListener(
-      "submit",
-      (event) => {
-
-        event.preventDefault();
-
-        formSuccess.classList.add(
-          "is-visible"
-        );
-
-      }
-    );
-
-  }
-
 });
